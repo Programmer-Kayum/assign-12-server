4ca2fd4e22f961fcaa5dbb0e607f1192352c2bd4
unknown <mohammadkayumislam@gmail.com>
2022-10-27 21:56:06 +0600

    last

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const course = require("./courses.json");
+app.get("/", (req, res) => {
+  res.send("api running!");
+});
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedItem = course.find((p) => p.id == id);
+  res.send(selectedItem);
+});
+
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`);
+});
+
+module.exports = app;
